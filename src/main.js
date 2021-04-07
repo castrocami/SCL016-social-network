56b10dc9412719f5ef45b4539faf94321e4b57ea
AleOlea <espiritusdelfuego@gmail.com>
2021-04-07 12:44:26 -0400

    Feat: Create components and views

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
-import {loginForm} from "./components/login.js";
-import {initForm} from "./lib/auth.js";
-
+//DOM
+import { addPost } from "./views.js/addPost.js";
+import { home } from "./views.js/home.js";
+import { loginForm } from "./views.js/login.js";
+import { profile } from "./views.js/profile.js";
+import { initForm } from "./components/auth.js";
+import { logOutForm } from "./views.js/logout.js";
 
 const content = document.getElementById("root");
-content.appendChild(loginForm());
-initForm();
+
+//Creating app routes
+
+switch (window.location.pathname) {
+    case "/login":
+        content.appendChild(loginForm());
+        initForm();
+        break;
+
+    case "/logOutForm":
+        content.appendChild(logOutForm());
+        break;
+
+    case "/home":
+        content.appendChild(home());
+        break;
+
+    case "/profile":
+        content.appendChild(profile());
+        break;
+
+    case "/addPost":
+        content.appendChild(addPost());
+        break;
+
+    default:
+        //
+        content.innerHTML = "No value found";
+}
---
+++ src/views.js/profile.js
@@ -0,0 +1,7 @@
+export const profile = () => {
+    const profile = document.createElement("H1");
+    profile.textContent = "Profile Page";
+    profile.id = "profile";
+
+    return profile;
+};
