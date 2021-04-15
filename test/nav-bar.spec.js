2c6dfec24a60a1eb52529efc948d63f67b5c3b50
Camila Castro <ccastromn@gmail.com>
2021-04-15 08:42:11 -0300

    Style: formatting correction

--- test/nav-bar.spec.js
+++ test/nav-bar.spec.js
@@ -1,9 +1,8 @@
 import { navBar } from '../src/components/nav-bar';
 
 describe('nav-bar', () => {
-
-    it('should render without crashing', () => {
-        const navB = navBar();
-        expect(navB instanceof HTMLElement).toBe(true);
-    });
+  it('should render without crashing', () => {
+    const navB = navBar();
+    expect(navB instanceof HTMLElement).toBe(true);
+  });
 });
