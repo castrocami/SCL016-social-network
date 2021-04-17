e27e91e17f9449c71aa4ed1857b3e907fc747cef
PamePebess <pamepebess07@gmail.com>
2021-04-17 18:47:09 -0400

    Feat: ProfilePost + test

--- src/views.js/profile.js
+++ src/views.js/profile.js
@@ -1,6 +1,8 @@
 import { navBar } from '../components/nav-bar.js';
 import { userData } from '../components/user-data.js';
 import { editOptions } from '../components/edit-delete.js';
+import { postProfile } from '../components/post-profile.js';
+
 
 export const profile = () => {
   const profileEl = document.createElement('div');
@@ -8,5 +10,7 @@ export const profile = () => {
   profileEl.appendChild(navBar());
   profileEl.appendChild(userData());
   profileEl.appendChild(editOptions());
+  profileEl.appendChild(postProfile());
+
   return profileEl;
 };
---
+++ test/userwelcome.spec.js
@@ -0,0 +1,8 @@
+import { currentUserWelcome } from '../src/components/userwelcome.js';
+
+describe('userwelcome', () => {
+  it('should render without crashing', () => {
+  const el = currentUserWelcome();
+  expect(el instanceof HTMLElement).toBe(true);
+  });
+});
