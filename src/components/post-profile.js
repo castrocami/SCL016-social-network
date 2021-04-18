ba80c40324e93c2b4fbf199b5b7f507ae4eadc33
AleOlea <espiritusdelfuego@gmail.com>
2021-04-18 10:33:29 -0400

    featIn post-profile edit-delete creation + UI. In delete-edit file all comented

--- src/components/post-profile.js
+++ src/components/post-profile.js
@@ -5,7 +5,7 @@ export const postProfile = () => {
     const profilePostDiv = document.createElement('div');
     profilePostDiv.id = 'profilePosts-div';
     const db = firebase.firestore();
-    // Create a reference to the cities collection
+    // Create a reference to the collection
     const currentUserPost = db.collection('allPosts');
 
 // Create a query against the collection.
@@ -16,14 +16,40 @@ export const postProfile = () => {
         querySnapshot.forEach((doc) => {
         const profilePosts = doc.data();
         console.log(profilePosts);
-        const profilePostsLi = document.createElement('li');
-        const profilePostsContainer = `<div>${profilePosts.tittle}</div><div>${profilePosts.content}</div>`;
-        profilePostsLi.innerHTML += profilePostsContainer;
-        profilePostDiv.appendChild(profilePostsLi);
-        console.log(doc.id, " => ", doc.data());
+
+    const postContainer = document.createElement('div');
+    postContainer.id ="post-container"
+
+    const profilePostsLi = document.createElement('li');
+    profilePostsLi.id = "profile-post-li"
+    const profilePostsContainer = `<div>${profilePosts.tittle}</div><div>${profilePosts.content}</div>`;
+    profilePostsLi.innerHTML += profilePostsContainer;
+       
+    postContainer.appendChild(profilePostsLi)  
+    profilePostDiv.appendChild(postContainer);
+    console.log(doc.id, " => ", doc.data());
+
+    const editionMenu= document.createElement('ul');
+    editionMenu.id = 'edition-menu';
+  
+    const editLi= document.createElement('li');
+    editLi.id = 'edit-li';
+    editLi.textContent= "edit"
+    editionMenu.appendChild(editLi);
+
+    const deleteLi =document.createElement('li');
+    deleteLi.id = 'delete-li';
+    deleteLi.textContent= "delete"
+    editionMenu.appendChild(deleteLi);
+
+    postContainer.appendChild(editionMenu)
+
+    
 
         
         });
     });      
     return profilePostDiv; 
 };
+
+
