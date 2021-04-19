beeebfaa7d2c81c6a99a502b3a44bad13917c364
AleOlea <espiritusdelfuego@gmail.com>
2021-04-19 10:42:54 -0400

    Feat: Edite & Delete User Profile Posts

--- src/components/post-profile.js
+++ src/components/post-profile.js
@@ -22,6 +22,7 @@ export const postProfile = () => {
 
     const profilePostsLi = document.createElement('li');
     profilePostsLi.id = "profile-post-li"
+
     const profilePostsContainer = `<div>${profilePosts.tittle}</div><div>${profilePosts.content}</div>`;
     profilePostsLi.innerHTML += profilePostsContainer;
        
@@ -29,6 +30,8 @@ export const postProfile = () => {
     profilePostDiv.appendChild(postContainer);
     console.log(doc.id, " => ", doc.data());
 
+//EDITING CONTENT
+ ///editing a post https://firebase.google.com/docs/firestore/manage-data/add-data?hl=es-419
     const editionMenu= document.createElement('ul');
     editionMenu.id = 'edition-menu';
   
@@ -36,20 +39,51 @@ export const postProfile = () => {
     editLi.id = 'edit-li';
     editLi.textContent= "edit"
     editionMenu.appendChild(editLi);
+    const editingMyPosts =()=> {
+      const editMyPost = db.collection("allPosts").doc(doc.id);
+      return editMyPost.update({
+        tittle: "Estoy cambiando el título",
+        content: "Estoy cambiando el content"
+    
+      })
+      .then(() => {
+        console.log("Document successfully updated!");
+      })
+      .catch((error) => {
+        // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+      });
+    
+    }
+    editLi.addEventListener("click", editingMyPosts)
 
-    const deleteLi =document.createElement('li');
+   //DELETING CONTENT
+    const deleteLi =document.createElement('button');
     deleteLi.id = 'delete-li';
     deleteLi.textContent= "delete"
     editionMenu.appendChild(deleteLi);
-
     postContainer.appendChild(editionMenu)
 
-    
-
-        
-        });
-    });      
+    //Delete a Post
+    const db = firebase.firestore();  
+    let deleteMyPost = () => {
+    db.collection('allPosts').doc(doc.id).delete().then(() => {
+    //  profilePostsLi.textContent = "";
+    //  postContainer.innerHTML = "";
+     profilePostDiv.removeChild(postContainer);
+     
+     console.log('Document successfully deleted!');
+        }).catch((error) => {
+        console.error('Error removing document: ', error);
+      });
+}
+    deleteLi.addEventListener("click", deleteMyPost)
+    });
+});      
     return profilePostDiv; 
 };
 
+
+ 
+  
 
