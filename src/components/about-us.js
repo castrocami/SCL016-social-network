1ac97e8775a1e26dcfe978dacb622cf8153f8d58
Camila Castro <ccastromn@gmail.com>
2021-04-20 12:51:41 -0300

    Style: Change in docs , eslint problems

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -1,10 +1,10 @@
-import { footer } from '../components/footer.js';
+import { footer } from './footer.js';
 // import {home}from "../views.js/"
 export const aboutUs = () => {
   const woWeAre = document.createElement('div');
   woWeAre.id = 'woWeAre';
 
   woWeAre.appendChild(footer());
-   
+
   return woWeAre;
 };
--- test/userwelcome.spec.js
+++ test/userwelcome.spec.js
@@ -2,7 +2,7 @@ import { currentUserWelcome } from '../src/components/userwelcome.js';
 
 describe('userwelcome', () => {
   it('should render without crashing', () => {
-  const el = currentUserWelcome();
-  expect(el instanceof HTMLElement).toBe(true);
+    const el = currentUserWelcome();
+    expect(el instanceof HTMLElement).toBe(true);
   });
 });
